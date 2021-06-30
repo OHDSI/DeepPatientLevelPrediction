7278648c8ad90078e3009ae7e96af04e5dd4fe2a
Henrik John <l.john@erasmusmc.nl>
2021-06-30 22:50:50 +0200

    Add temporary file location for uncompressed SQLite database

--- inst/python/read_write_plp_data.py
+++ inst/python/read_write_plp_data.py
@@ -17,6 +17,7 @@ directly from Python to avoid loading data through R."""
 import zipfile
 from pathlib import Path
 import warnings
+import tempfile
 
 # --------------------------------------------------------------------------- #
 #                  OTHER IMPORTS                                              #
@@ -57,6 +58,8 @@ class ReadWritePlpData:
         self.meta_data = None
         self.time_ref = None
         self.cov_rds = None
+        self.population = None
+        self.tf = tempfile.TemporaryDirectory()
 
 # --------------------------------------------------------------------------- #
 #                  EXPORTED FUNCTIONS                                         #
@@ -66,13 +69,32 @@ class ReadWritePlpData:
         :param path: Path to Andromeda object
         :return:
         """
-        sqlite_path = self.__uncompress_covariates(Path(path, "covariates"))
-        self.__create_sqlite_engine(sqlite_path)
-        self.__read_rds_files(path, sqlite_path)
-        # __test_sqlite_connection()
+        self.__uncompress_covariates(path)
+        self.__create_sqlite_engine(self.tf.name)
+        self.__read_rds_files(path, self.tf.name)
+
+    def load_population(self, path):
+        """Load population from .RDS object
+        :param path: Path to population .RDS object
+        :return:
+        """
+        print("Loading population .RDS..")
+        try:
+            self.population = (pyreadr.read_r(str(Path(path))))[None]
+        except pyreadr.custom_errors.LibrdataError as err:
+            print("Handling LibrdataError: Population could not be read.", err)
+
+    def get_population(self):
+        """Returns population
+        :return: Population
+        """
+        if self.population is not None:
+            return self.population
+        warnings.warn("No population loaded. First run load_population().")
+        return None
 
     def get_covariates(self):
-        """Get covariates through SQLite query
+        """Returns covariates through SQLite query
         :return: Pandas DataFrame holding covariates
         """
         if self.db_engine is not None:
@@ -83,7 +105,7 @@ class ReadWritePlpData:
         return None
 
     def get_covariate_ref(self):
-        """Get covariateRef through SQLite query
+        """Returns covariateRef through SQLite query
         :return: Pandas DataFrame holding covariateRef
         """
         if self.db_engine is not None:
@@ -94,14 +116,14 @@ class ReadWritePlpData:
         return None
 
     def get_analysis_ref(self):
-        """Get analysisRef through SQLite query
+        """Returns analysisRef through SQLite query
         :return: Pandas DataFrame holding analysisRef
         """
         if self.db_engine is not None:
             analysis_ref = pd.read_sql('select * from analysisRef',
                                        self.db_engine)
             return analysis_ref
-        warnings.warn("No connection to SQLite. First run load_plp_data().")
+        warnings.warn("No connection to SQLite. First srun load_plp_data().")
         return None
 
     def custom_query(self, query):
@@ -117,17 +139,27 @@ class ReadWritePlpData:
                   " executed.", err)
             return None
 
+    def get_tables(self):
+        """Returns tables present in Andromeda SQLite database
+        :return:
+        """
+        inspector = sqlalchemy.inspect(self.db_engine)
+        inspect_tables = inspector.get_table_names()
+        print('Tables: %s' % inspect_tables)
+        return inspect_tables
+
 # --------------------------------------------------------------------------- #
 #                  LOCAL FUNCTIONS                                            #
 # --------------------------------------------------------------------------- #
-    def __uncompress_covariates(self, path_to_covariates):
+    def __uncompress_covariates(self, path):
         """Uncompress SQLite database within an Andromeda object
-        :return: Path to unzipped SQLite database holding covariate data"""
+        :param path: Path to Andromeda object
+        :return:
+        """
         print("Uncompressing data files..")
-        sqlite_path = Path(Path.cwd(), "plpDataTemp")
+        path_to_covariates = Path(path, "covariates")
         with zipfile.ZipFile(path_to_covariates, 'r') as zip_ref:
-            zip_ref.extractall(sqlite_path)
-        return sqlite_path
+            zip_ref.extractall(self.tf.name)
 
     def __create_sqlite_engine(self, sqlite_path):
         """Create the database engine to query SQLite database
@@ -139,28 +171,6 @@ class ReadWritePlpData:
         self.db_engine = sqlalchemy.create_engine(
             'sqlite:///' + str(rel_sqlite_db_path[0]))
 
-    def __test_sqlite_connection(self):
-        """Just a testing function, which will be expanded or removed later
-        :return:
-        """
-        inspector = sqlalchemy.inspect(self.db_engine)
-        inspect_tables = inspector.get_table_names()
-        print('Tables: %s' % inspect_tables)
-        print(self.outcomes[None])
-        # Read data with pandas
-        print(pd.read_sql('select * from covariateRef', self.db_engine))
-        # print(outcomes.keys())
-        # print(outcomes[None])
-
-        # print(cohorts.keys())
-        # print(cohorts[None])
-
-        # print(time_ref.keys())
-        # print(time_ref[None])
-
-        # print(cov_rds.keys())
-        # print(cov_rds[None])
-
     def __read_rds_files(self, path, sqlite_path):
         """Read .RDS files from Andromeda object
         :param path: Path to Andromeda object
@@ -170,25 +180,26 @@ class ReadWritePlpData:
         print("Reading .RDS files..")
 
         try:
-            self.outcomes = pyreadr.read_r(Path(path, "outcomes.rds"))
+            self.outcomes = pyreadr.read_r(str(Path(path,
+                                                    "outcomes.rds")))[None]
         except pyreadr.custom_errors.LibrdataError as err:
             print("Handling LibrdataError: Andromeda file outcomes.rds could" +
                   " not be read.", err)
 
         try:
-            self.cohorts = pyreadr.read_r(Path(path, "cohorts.rds"))
+            self.cohorts = pyreadr.read_r(str(Path(path, "cohorts.rds")))[None]
         except pyreadr.custom_errors.LibrdataError as err:
             print("Handling LibrdataError: Andromeda file cohorts.rds could" +
                   " not be read.", err)
 
         try:
-            self.meta_data = pyreadr.read_r(Path(path, "metaData.rds"))
+            self.meta_data = pyreadr.read_r(str(Path(path, "metaData.rds")))
         except pyreadr.custom_errors.LibrdataError as err:
             print("Handling LibrdataError: Andromeda file metaData.rds could" +
                   " not be read.", err)
 
         try:
-            self.time_ref = pyreadr.read_r(Path(path, "timeRef.rds"))
+            self.time_ref = pyreadr.read_r(str(Path(path, "timeRef.rds")))
         except pyreadr.custom_errors.LibrdataError as err:
             print("Handling LibrdataError: Andromeda file timeRef.rds could" +
                   " not be read.", err)
@@ -197,7 +208,7 @@ class ReadWritePlpData:
             self.cov_rds = pyreadr.read_r(str(list(Path(sqlite_path)
                                                    .glob("*.rds"))[0]))
         except pyreadr.custom_errors.LibrdataError as err:
-            print("Handling LibrdataError: Andromeda file outcomes.rds could" +
+            print("Handling LibrdataError: Andromeda file sqlite.rds could" +
                   " not be read.", err)
 
 # --------------------------------------------------------------------------- #
