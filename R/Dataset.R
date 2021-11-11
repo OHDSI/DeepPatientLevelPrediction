ebc2ca5b22219008ba82d79e1d273cd7bb0d8ef3
Egill Fridgeirsson <egillax@gmail.com>
2021-11-11 16:47:23 +0100

    changed dataset to use sparse COO matrix

--- R/Dataset.R
+++ R/Dataset.R
@@ -7,15 +7,19 @@ Dataset <- torch::dataset(
     self$target <- torch::torch_tensor(labels[indices])
     
     # add features
-    #print(dim(as.matrix(data[indices,]))) ## testing
-    # TODO should be torch sparse COO matrix
-    self$cat <- torch::torch_tensor(as.matrix(data[indices,-numericalIndex, drop = F]), dtype=torch::torch_float32())
+    dataCat <- data[indices,-numericalIndex]
+    matrix <- as(dataCat, 'dgTMatrix') # convert to triplet sparse format
+    sparseIndices <- torch::torch_tensor(matrix(c(matrix@i + 1, matrix@j + 1), ncol=2), dtype = torch::torch_long())
+    values <- torch::torch_tensor(matrix(c(matrix@x)), dtype = torch::torch_float32())
+    self$cat <- torch::torch_sparse_coo_tensor(indices=sparseIndices$t(), 
+                                               values=values$squeeze(), 
+                                               dtype=torch::torch_float32())$coalesce()
     self$num <- torch::torch_tensor(as.matrix(data[indices,numericalIndex, drop = F]), dtype=torch::torch_float32())
     
   },
   
   .getitem = function(item) {
-    return(list(cat = self$cat[item,], 
+    return(list(cat = self$cat[item]$to_dense(), 
                 num = self$num[item,],
                 target = self$target[item]))
   },
