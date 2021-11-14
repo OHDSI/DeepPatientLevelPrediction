9c44d8c62f84e231d8802dae10f6a1d66ea00841
Egill Fridgeirsson <egillax@gmail.com>
2021-11-14 16:19:59 +0100

    Added a collate fn, so instead of changing each sample to_dense and then stacking in batch, it first stacks the sparse tensors and then calls to_dense

--- R/Dataset.R
+++ R/Dataset.R
@@ -1,7 +1,7 @@
 Dataset <- torch::dataset(
   name = 'Dataset',
   
-  initialize=function(data, labels, indices, numericalIndex) {
+  initialize = function(data, labels, indices, numericalIndex) {
     
     # add labels
     self$target <- torch::torch_tensor(labels[indices])
@@ -15,11 +15,10 @@ Dataset <- torch::dataset(
                                                values=values$squeeze(), 
                                                dtype=torch::torch_float32())$coalesce()
     self$num <- torch::torch_tensor(as.matrix(data[indices,numericalIndex, drop = F]), dtype=torch::torch_float32())
-    
   },
   
   .getitem = function(item) {
-    return(list(cat = self$cat[item]$to_dense(), 
+    return(list(cat = self$cat[item], 
                 num = self$num[item,],
                 target = self$target[item]))
   },
@@ -28,3 +27,30 @@ Dataset <- torch::dataset(
     self$target$size()[[1]] # shape[1]
   }
 )
+
+# a function to speed up the collation so I dont' call to_dense()
+# on the sparse tensors until they have been combined for the batch
+sparseCollate <- function(batch) {
+  browser()
+  elem <- batch[[1]]
+  if (inherits(elem, "torch_tensor")) {
+    # temporary fix using a tryCatch until torch in R author adds
+    # an is_sparse method or exposes tensor&layout
+    tryCatch(return(torch::torch_stack(batch,dim = 1)$to_dense()),
+             error=function(e) return(torch::torch_stack(batch, dim = 1)))  
+      
+  # if (Reduce("*", elem$size()) > elem$numel()) {
+  #     return(torch::torch_stack(batch,dim = 1)$to_dense())
+  #   }
+  #   
+  #   return(torch::torch_stack(batch, dim = 1))
+  }
+  else if (is.list(elem)) {
+   
+    # preserve names of elements 
+    named_seq <- seq_along(elem)
+    names(named_seq) <- names(elem)
+    
+    lapply(named_seq, function(i) {sparseCollate(lapply(batch, function(x) x[[i]]))})
+  }
+}
