f569fb74670e66b39717e57e3f056a3d8ee03998
Egill Fridgeirsson <egillax@gmail.com>
2022-01-26 11:21:25 +0100

    dataset no longer used indices since train-test split is handled by subsets

--- R/Dataset.R
+++ R/Dataset.R
@@ -1,38 +1,38 @@
+#' @export
 Dataset <- torch::dataset(
   name = 'Dataset',
   
-  initialize = function(data, labels, indices, numericalIndex) {
+  initialize = function(data, labels, numericalIndex) {
     
-    self$collate_fn <- sparseCollate  
+    # self$collate_fn <- sparseCollate  
     # add labels
-    self$target <- torch::torch_tensor(labels[indices])
+    self$target <- torch::torch_tensor(labels)
     
     # Weight to add in loss function to positive class
     self$posWeight <- ((self$target==0)$sum()/self$target$sum())$item()
     
     # add features
-    dataCat <- data[indices,-numericalIndex]
+    dataCat <- data[,-numericalIndex]
     self$cat <- torch::torch_tensor(as.matrix(dataCat), dtype=torch::torch_float32())
-        
+    
+            
     # comment out the sparse matrix for now, is really slow need to find 
     # a better solution for converting it to dense before feeding to model
     # matrix <- as(dataCat, 'dgTMatrix') # convert to triplet sparse format
     # sparseIndices <- torch::torch_tensor(matrix(c(matrix@i + 1, matrix@j + 1), ncol=2), dtype = torch::torch_long())
     # values <- torch::torch_tensor(matrix(c(matrix@x)), dtype = torch::torch_float32())
-    # self$cat <- torch::torch_sparse_coo_tensor(indices=sparseIndices$t(), 
-    #                                            values=values$squeeze(), 
+    # self$cat <- torch::torch_sparse_coo_tensor(indices=sparseIndices$t(),
+    #                                            values=values$squeeze(),
     #                                            dtype=torch::torch_float32())$coalesce()
-    self$num <- torch::torch_tensor(as.matrix(data[indices,numericalIndex, drop = F]), dtype=torch::torch_float32())
+    self$num <- torch::torch_tensor(as.matrix(data[,numericalIndex, drop = F]), dtype=torch::torch_float32())
   },
-  
-  .getitem = function(item) {
-    return(list(cat = self$cat[item], 
-                num = self$num[item,],
+  .getbatch = function(item) {
+    return(list(cat = self$cat[item],
+                num = self$num[item],
                 target = self$target[item]))
   },
-  
   .length = function() {
-    self$target$size()[[1]] # shape[1]
+    length(self$target) # shape[1]
   }
 )
 
@@ -43,17 +43,11 @@ sparseCollate <- function(batch) {
   if (inherits(elem, "torch_tensor")) {
     # temporary fix using a tryCatch until torch in R author adds
     # an is_sparse method or exposes tensor$layout
-    tryCatch(return(torch::torch_stack(batch,dim = 1)$to_dense()),
-             error=function(e) return(torch::torch_stack(batch, dim = 1)))  
-      
-  # if (Reduce("*", elem$size()) > elem$numel()) {
-  #     return(torch::torch_stack(batch,dim = 1)$to_dense())
-  #   }
-  #   
-  #   return(torch::torch_stack(batch, dim = 1))
+    return (torch::torch_stack(batch, dim = 1))
+    # tryCatch(return(torch::torch_stack(batch,dim = 1)$to_dense()),
+    #          error=function(e) return(torch::torch_stack(batch, dim = 1)))  
   }
   else if (is.list(elem)) {
-   
     # preserve names of elements 
     named_seq <- seq_along(elem)
     names(named_seq) <- names(elem)
@@ -61,3 +55,11 @@ sparseCollate <- function(batch) {
     lapply(named_seq, function(i) {sparseCollate(lapply(batch, function(x) x[[i]]))})
   }
 }
+
+
+
+
+
+
+
+
