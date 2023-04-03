5458a033257b8f0ce4a89f66534ca1a39c05b4b3
Henrik John <l.john@erasmusmc.nl>
2023-04-03 12:54:01 +0200

    Update tests

--- tests/testthat/test-Transformer.R
+++ tests/testthat/test-Transformer.R
@@ -27,7 +27,7 @@ test_that("Transformer settings work", {
 })
 
 test_that("fitEstimator with Transformer works", {
-  results <- fitEstimator(trainData$Train, settings, analysisId = 1)
+  results <- fitEstimator(trainData$Train, settings, analysisId = 1, analysisPath = testLoc)
 
   expect_equal(class(results), "plpModel")
   expect_equal(attr(results, "modelType"), "binary")
